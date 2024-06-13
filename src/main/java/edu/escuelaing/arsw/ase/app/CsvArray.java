5ed20ba34b1d91d738759b3f73b22654a80f0344
paralol <santinico36@hotmail.com>
2024-06-13 12:33:54 -0500

    lee csv

---
+++ src/main/java/edu/escuelaing/arsw/ase/app/CsvArray.java
@@ -0,0 +1,24 @@
+package edu.escuelaing.arsw.ase.app;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CsvArray {
+    static List<List<String>> records = new ArrayList<>();
+    public static List<List<String>> getCsv() throws FileNotFoundException, IOException{
+
+        try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+        }
+        return records;
+    }
+}
